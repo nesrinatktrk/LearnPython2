77edd990ce5c8c8b2b0ab3f41016c493f3698df9
Nesrin Atiktürk <79600556+nesrinatktrk@users.noreply.github.com>
2022-05-11 17:03:53 +0300

    Rename kopya_klasor3.py to shutil.py

--- kopya_klasor3.py
+++
@@ -1,5 +0,0 @@
-import shutil
-import os
-
-
-shutil.copytree("C:/Users/ogr_8/Desktop/fotoğraflar","fotoğraflar")
---
+++ shutil.py
@@ -0,0 +1,5 @@
+import shutil
+import os
+
+
+shutil.copytree("C:/Users/ogr_8/Desktop/fotoğraflar","fotoğraflar")
