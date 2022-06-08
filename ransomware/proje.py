c58176cf58f2ac1d1161e320ca2eab5dade1c858
Nesrin Atiktürk <79600556+nesrinatktrk@users.noreply.github.com>
2022-06-08 18:21:04 +0300

    Update proje.py

---
+++ ransomware/proje.py
@@ -0,0 +1,3 @@
+#Kullanıcıdan şifrelemek istediği dosyanın adını alıp şifreleyerek şifrelediği keyi kullanıcıya not etmesi için veren başka bir yere kaydetmeyen programı yazınız. Programın şifre çözme seçeneğinde de kullanıcıdan keyi ve dosyayı isteyerek şifresini çözen uygulama 
+#Bu programı klasör bazlı yaz
+#Hazırladığınız programı python tkinder kütüphanesini kullanarak yazın görsel bir programa çevir
